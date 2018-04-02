db85491cc4ea367ec7fb260e1f928c57ad670323
Jared Ledvina <jared@techsmix.net>
2018-04-02 09:39:23 -0400

    Fuck doing this

--- providers.tf
+++ providers.tf
@@ -1,14 +1,10 @@
 provider "aws" {
-  access_key = "${var.aws_access_key}"
-  secret_key = "${var.aws_secret_key}"
-  region     = "${var.region}"
-  version    = "~> 1.13.0"
+  region  = "${var.region}"
+  version = "~> 1.13.0"
 }
 
 provider "aws" {
-  alias      = "east"
-  access_key = "${var.aws_access_key}"
-  secret_key = "${var.aws_secret_key}"
-  region     = "us-east-1"
-  version    = "~> 1.13.0"
+  alias   = "east"
+  region  = "us-east-1"
+  version = "~> 1.13.0"
 }
--- variables.tf
+++ variables.tf
@@ -1,13 +1,3 @@
-variable "aws_access_key" {
-  type        = "string"
-  description = "AWS Access Key"
-}
-
-variable "aws_secret_key" {
-  type        = "string"
-  description = "AWS Secret Key"
-}
-
 variable "region" {
   type        = "string"
   description = "AWS Region"
